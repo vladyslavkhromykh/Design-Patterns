85a70e23d7ecd5434d5bec7d6f55f9a3fd90d912
Vladyslav Khromykh <vladyslav.khromykh@tellurionmobile.com>
2019-10-08 15:27:58 +0300

    Added Prorotype design pattern implementation.

---
+++ Prototype/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Prototype
+{
+    public class Person : IPrototype<Person>
+    {
+        private string name;
+        private int age;
+
+        public override string ToString()
+        {
+            return $"Name: {name}, age: {age}";
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person Clone()
+        {
+            var person = new Person(this.name, this.age);
+            return person;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person("John", 25);
+            
+            Console.WriteLine("Here is our new person John:");
+            Console.WriteLine(john);
+
+            Person johnClone = john.Clone();
+            
+            Console.WriteLine("Here is our John clone :");
+            Console.WriteLine(john);
+            
+            Console.WriteLine("They both are look the same, are they not?");
+        }
+    }
+}
