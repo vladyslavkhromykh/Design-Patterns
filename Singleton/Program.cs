8befcd82aa0b2c5c5fa94415ba31709d2b60ba7e
Vladyslav Khromykh <vladyslav.khromykh@tellurionmobile.com>
2019-10-08 18:52:36 +0300

    Added Singleton design pattern implementation.

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonService = SingletonService.Instance;
+            // Do whatever you want with single entry point with no ability to change it.
+        }
+    }
+}
---
+++ Singleton/SingletonService.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    public class SingletonService
+    {
+        private static SingletonService instance;
+
+        public static SingletonService Instance => instance ?? (instance = new SingletonService());
+
+        private SingletonService()
+        {
+            
+        }
+    }
+}
