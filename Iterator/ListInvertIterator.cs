96852106e7b9e329ff3b81c4d1c98b039f032e25
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-12-05 22:40:43 +0200

    Added Iterator design pattern implementation.

---
+++ Iterator/ListInvertIterator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class ListInvertIterator<T>
+    {
+        private List<T> list;
+        
+        public ListInvertIterator(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                yield return list[i];
+            }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>
+            {
+                5, 103, 44, 255, 900, 42, 167, 433
+            };
+            
+            ListInvertIterator<int> invertIterator = new ListInvertIterator<int>(numbers);
+
+            foreach (var i in invertIterator)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
