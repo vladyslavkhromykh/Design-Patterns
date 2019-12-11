b9b36bf589f5e0fef98ee3a07187afb9771816c5
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-12-11 22:08:23 +0200

    Added Memento design pattern implementation.

---
+++ Memento/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Memento
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TextEditor editor = new TextEditor();
+            editor.AddLine("First line of code");
+            editor.AddLine("Second line of code");
+            editor.AddLine("Third line of code");
+
+            Console.WriteLine("Text editor content:");
+            Console.WriteLine(editor.GetContent());
+            
+            TextEditor.Snapshot snapshot = editor.GetSnapshot();
+            
+            editor.AddLine("Fourth line of code");
+            
+            Console.WriteLine("Text editor content:");
+            Console.WriteLine(editor.GetContent());
+
+            editor.Restore(snapshot);
+            
+            Console.WriteLine("Text editor content:");
+            Console.WriteLine(editor.GetContent());
+        }
+    }
+}
---
+++ Memento/TextEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace Memento
+{
+    public class TextEditor
+    {
+        private int line;
+        private int carriagePosition;
+        private StringBuilder builder;
+        
+        public struct Snapshot
+        {
+            private int line;
+            private int carriagePosition;
+            private string content;
+
+            public int Line
+            {
+                get { return this.line; }
+            }
+
+            public int CarriagePosition
+            {
+                get { return this.carriagePosition; }
+            }
+            
+            public string Content
+            {
+                get { return this.content; }
+            }
+            
+            public Snapshot(TextEditor editor)
+            {
+                this.line = editor.line;
+                this.carriagePosition = editor.carriagePosition;
+                this.content = editor.builder.ToString();
+            }
+        }
+
+        public TextEditor()
+        {
+            this.builder = new StringBuilder();
+        }
+
+        public Snapshot GetSnapshot()
+        {
+            return new Snapshot(this);
+        }
+
+        public void Restore(Snapshot snapshot)
+        {
+            this.line = snapshot.Line;
+            this.carriagePosition = snapshot.CarriagePosition;
+            this.builder = new StringBuilder(snapshot.Content);
+        }
+
+        public string GetContent()
+        {
+            return this.builder.ToString();
+        }
+
+        public void AddLine(string text)
+        {
+            StringBuilder line = new StringBuilder(text);
+            line.AppendLine();
+            this.builder.Append(line);
+        }
+    }
+}
