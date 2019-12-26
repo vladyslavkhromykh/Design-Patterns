2b55e3d7088cb07f6fbd822e7f5cea4c19a504b1
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2019-12-26 21:10:15 +0200

    Added Strategy design pattern implementation.

---
+++ Strategy/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    public class Player
+    {
+        private string name;
+        private int hash;
+        
+        public Player(string name, int hash)
+        {
+            this.name = name;
+            this.hash = hash;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Name: {name}. Hash: {hash}.");
+        }
+
+        public class NameComparer : IComparer<Player>
+        {
+            public int Compare(Player x, Player y)
+            {
+                return String.CompareOrdinal(x.name, y.name);
+            }
+        }
+        
+        public class HashComparer : IComparer<Player>
+        {
+            public int Compare(Player x, Player y)
+            {
+                if (x.hash > y.hash)
+                {
+                    return 1;
+                }
+
+                if (x.hash < y.hash)
+                {
+                    return -1;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Player john = new Player("John", 12);
+            Player ken = new Player("Ken", 6);
+            Player ben = new Player("Ben", 25);
+
+            List<Player> players = new List<Player>
+            {
+                john, ken, ben
+            };
+            
+            players.Sort(new Player.HashComparer());
+            players.ForEach(player => Console.WriteLine(player.ToString()));
+            
+            players.Sort(new Player.NameComparer());
+            players.ForEach(player => Console.WriteLine(player.ToString()));
+        }
+    }
+}
